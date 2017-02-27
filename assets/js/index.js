e632265fa9c12774103775baddd15aeea576618f
Gersain <gersigner@hotmail.com>
2017-02-27 16:42:36 -0600

    inregración vista previa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
+
+//∆∆ BTN COLLPASE
  $(".button-collapse").sideNav();
+
+ //∆∆ MODAL
  $('.modal').modal();
- // $('.grid').masonry({
- //  // options
- //  itemSelector: '.grid-item',
- //  columnWidth: 200;
- //  });
+
+     $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .8, // Opacity of modal background
+      inDuration: 500, // Transition in duration
+      outDuration: 500, // Transition out duration
+    }
+    )
