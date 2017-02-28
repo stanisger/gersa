e8e2b8ac3a79cc6492bec2d2be6becf834544b95
Gersain <gersigner@hotmail.com>
2017-02-28 17:07:20 -0600

    inregración vista previa 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,5 +10,46 @@
       opacity: .8, // Opacity of modal background
       inDuration: 500, // Transition in duration
       outDuration: 500, // Transition out duration
-    }
+     }
     )
+
+//∆∆ Modal&Objetos
+var modalSelect = $('.modal');
+var computer = $('#animate1');
+
+//∆∆ Altura de Objetos (computer)
+var objetHeight = function height(objet){  
+      return  objet.outerHeight();   
+ }
+
+
+//∆∆ Scroll (modalSelect)
+var pixels = function height(obj){
+	return obj.scrollTop();
+}
+
+$(document).scroll(function(){
+
+	function ecuation(height){
+	    return (height + pixels(modalSelect)) / height;
+	  }
+    
+
+
+    var animation = function animation(multi){
+       return ecuation(objetHeight(computer)) * multi;
+        
+    }
+
+    if(pixels(modalSelect) >= 1){
+        computer.css({ 
+            "top" : animation(10)  + "px", 
+            "display" : "inherit"
+               }
+            
+         );
+    }
+
+});
+	
+
