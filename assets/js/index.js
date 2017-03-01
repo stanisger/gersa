842ecd96e78cad4f65974292932d8a31e777bae7
Gersain <gersigner@hotmail.com>
2017-03-01 16:53:51 -0600

    inregración vista previa 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,49 +7,51 @@
 
      $('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .8, // Opacity of modal background
+      opacity: .6, // Opacity of modal background
       inDuration: 500, // Transition in duration
       outDuration: 500, // Transition out duration
      }
     )
 
-//∆∆ Modal&Objetos
-var modalSelect = $('.modal');
-var computer = $('#animate1');
+// //∆∆ Modal&Objetos
+// var modalSelect = $('.modal');
+// var computer = $('#animate1');
 
-//∆∆ Altura de Objetos (computer)
-var objetHeight = function height(objet){  
-      return  objet.outerHeight();   
- }
+// //∆∆ Altura de Objetos (computer)
+// var objetHeight = function height(objet){  
+//       return  objet.outerHeight();   
+//  }
 
 
-//∆∆ Scroll (modalSelect)
-var pixels = function height(obj){
-	return obj.scrollTop();
-}
+// //∆∆ Scroll (modalSelect)
+// var pixels = function height(obj){
+// 	return obj.scrollTop();
+// }
 
-$(document).scroll(function(){
+// $(document).scroll(function(){
 
-	function ecuation(height){
-	    return (height + pixels(modalSelect)) / height;
-	  }
+
+
+// 	// function ecuation(height){
+// 	//     return (height + pixels(modalSelect)) / height;
+// 	//   }
     
 
 
-    var animation = function animation(multi){
-       return ecuation(objetHeight(computer)) * multi;
+//  //    var animation = function animation(multi){
+//  //       return ecuation(objetHeight(computer)) * multi;
         
-    }
+//  //    }
 
-    if(pixels(modalSelect) >= 1){
-        computer.css({ 
-            "top" : animation(10)  + "px", 
-            "display" : "inherit"
-               }
+//  //    if(pixels(modalSelect) >= 1){
+//  //        computer.css({ 
+//  //            "top" : animation(10)  + "px", 
+//  //            "display" : "inherit"
+//  //               }
             
-         );
-    }
+//  //         );
+//  //    }
 
-});
+// });
 	
 
